4b120885cff0b57251787dd6eb8cddf5e7ac5c91
Mike Anderson <athaeryn@me.com>
2013-03-08 16:26:27 -0600

    Made the bclose script installation part of the viminstall script.

--- viminstall.sh
+++ viminstall.sh
@@ -2,17 +2,14 @@
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl 'www.vim.org/scripts/download_script.php?src_id=16224' > ~/.vim/autoload/pathogen.vim
+mkdir -p ~/.vim/bundle/bclose/plugin
+cp bclose.vim ~/.vim/bundle/bclose/plugin/bclose.vim
 cd ~/.vim/bundle
 git clone https://github.com/altercation/vim-colors-solarized.git
 git clone https://github.com/hallison/vim-markdown.git
 git clone https://github.com/kien/ctrlp.vim.git
 git clone git://github.com/tpope/vim-commentary.git
 git clone https://github.com/mileszs/ack.vim.git
-# Define :Bclose to close a buffer without closing a window using
-# the first script on
-# http://vim.wikia.com/wiki/Deleting_a_buffer_without_closing_the_window
-# and placing it in
-# ~/.vim/bundle/bclose/plugin/bclose.vim
 git clone https://github.com/tpope/vim-repeat.git 
 git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/scrooloose/syntastic.git
