917fd29ab0fd081538ce71a2b1b24c1ac55f88a8
Mike Anderson <athaeryn@me.com>
2013-03-15 10:18:54 -0500

    Reintroduced NERDCommenter. Also sorted the git clone block.

--- viminstall.sh
+++ viminstall.sh
@@ -5,16 +5,17 @@ curl 'www.vim.org/scripts/download_script.php?src_id=16224' > ~/.vim/autoload/pa
 mkdir -p ~/.vim/bundle/bclose/plugin
 cp bclose.vim ~/.vim/bundle/bclose/plugin/bclose.vim
 cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-commentary.git
 git clone https://github.com/altercation/vim-colors-solarized.git
 git clone https://github.com/hallison/vim-markdown.git
 git clone https://github.com/kien/ctrlp.vim.git
-git clone git://github.com/tpope/vim-commentary.git
 git clone https://github.com/mileszs/ack.vim.git
+git clone https://github.com/nathanaelkane/vim-indent-guides.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/syntastic.git
 git clone https://github.com/tpope/vim-repeat.git 
 git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/scrooloose/syntastic.git
-git clone https://github.com/nathanaelkane/vim-indent-guides.git
-git clone https://github.com/scrooloose/nerdtree.git
 mkdir -p ~/.vim/undo
 mkdir -p ~/.vim/backup
 
