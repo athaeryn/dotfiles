fcc291f018538797a632d4b2920b33eccfc05105
Mike Anderson <athaeryn@me.com>
2013-04-17 09:35:22 -0500

    Add cucumber.vim and format lines.

--- viminstall.rb
+++ viminstall.rb
@@ -1,23 +1,24 @@
 #!/usr/bin/env ruby
 
 plugins = {
-    "NrrwRgn" => "git@github.com:chrisbra/NrrwRgn.git",
-    "ack.vim" => "https://github.com/mileszs/ack.vim.git",
-    "ctrlp.vim" => "https://github.com/kien/ctrlp.vim.git",
-    "delimitMate" => "git@github.com:Raimondi/delimitMate.git",
-    "nerdcommenter" => "https://github.com/scrooloose/nerdcommenter.git",
-    "nerdtree" => "https://github.com/scrooloose/nerdtree.git",
-    "syntastic" => "https://github.com/scrooloose/syntastic.git",
-    "tabular" => "git://github.com/godlygeek/tabular.git",
-    "vim-characterize" => "git://github.com/tpope/vim-characterize.git",
+    "NrrwRgn"              => "git@github.com:chrisbra/NrrwRgn.git",
+    "ack.vim"              => "https://github.com/mileszs/ack.vim.git",
+    "ctrlp.vim"            => "https://github.com/kien/ctrlp.vim.git",
+    "delimitMate"          => "git@github.com:Raimondi/delimitMate.git",
+    "nerdcommenter"        => "https://github.com/scrooloose/nerdcommenter.git",
+    "nerdtree"             => "https://github.com/scrooloose/nerdtree.git",
+    "syntastic"            => "https://github.com/scrooloose/syntastic.git",
+    "tabular"              => "git://github.com/godlygeek/tabular.git",
+    "vim-characterize"     => "git://github.com/tpope/vim-characterize.git",
     "vim-colors-solarized" => "https://github.com/altercation/vim-colors-solarized.git",
-    "vim-commentary" => "git://github.com/tpope/vim-commentary.git",
-    "vim-indent-guides" => "https://github.com/nathanaelkane/vim-indent-guides.git",
-    "vim-markdown" => "https://github.com/athaeryn/vim-markdown.git",
-    "vim-ragtag" => "https://github.com/tpope/vim-ragtag.git",
-    "vim-repeat" => "https://github.com/tpope/vim-repeat.git",
-    "vim-surround" => "https://github.com/tpope/vim-surround.git",
-    "vim-unimpaired" => "git://github.com/tpope/vim-unimpaired.git"
+    "vim-commentary"       => "git://github.com/tpope/vim-commentary.git",
+    "vim-cucumber"         => "git@github.com:tpope/vim-cucumber.git",
+    "vim-indent-guides"    => "https://github.com/nathanaelkane/vim-indent-guides.git",
+    "vim-markdown"         => "https://github.com/athaeryn/vim-markdown.git",
+    "vim-ragtag"           => "https://github.com/tpope/vim-ragtag.git",
+    "vim-repeat"           => "https://github.com/tpope/vim-repeat.git",
+    "vim-surround"         => "https://github.com/tpope/vim-surround.git",
+    "vim-unimpaired"       => "git://github.com/tpope/vim-unimpaired.git"
 }
 
 puts "Getting ready..."
