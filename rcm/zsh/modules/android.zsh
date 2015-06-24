59490f6106ecc8c7debfbe54bbf658ae48a5d4a3
Mike Anderson <hello@mrmikea.com>
2015-06-24 12:28:50 -0500

    Put all the zsh stuff in one file
    "modules" was a dumb idea.

--- rcm/zsh/modules/android.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# ANDROID
-export PATH=$PATH:~/android/android-sdk-macosx/tools
-export PATH=$PATH:~/android/android-sdk-macosx/platform-tools
-export ANDROID_HOME=~/android/android-sdk-macosx
--- rcm/zsh/modules/gource.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-function dimensions_for_gource {
-    ruby -e 'puts `system_profiler SPDisplaysDataType | grep Resolution`.lines[0][22..-1].tr(" ", "")'
-}
-# always load gource config ~/.gource
-alias gource="gource --load-config ~/.gourcerc"
-_D=$(dimensions_for_gource)
-alias gourcefull="gource --load-config ~/.gourcerc -f -$_D"
