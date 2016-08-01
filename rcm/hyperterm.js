2d14b43e9fb40e8f8ea77dc33bb0fd5d1069cd67
Mike Anderson <hello@mrmikea.com>
2016-08-01 09:13:28 -0500

    Update iTerm prefs, add .hyperterm.js

---
+++ rcm/hyperterm.js
@@ -0,0 +1,43 @@
+module.exports = {
+  config: {
+    fontSize: 14,
+    fontFamily: '"Fira Mono", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+
+    cursorColor: 'rgba(0, 255, 255, 0.5)',
+    cursorShape: 'BLOCK',
+
+    foregroundColor: '#e7e3d9',
+    backgroundColor: '#1c1c1e',
+
+    borderColor: 'black',
+
+    // custom css
+    css: ``,
+    termCSS: ``,
+
+    padding: '0', // padding is for babies
+
+    colors: {
+      black: '#232323',
+      red: '#e07071',
+      green: '#bad14d',
+      yellow: '#f2b555',
+      blue: '#54b2dd',
+      magenta: '#ab83b8',
+      cyan: '#58b6a8',
+      white: '#a3a6a4',
+
+      lightBlack: '#787878',
+      lightRed: '#c94844',
+      lightGreen: '#a0a459',
+      lightYellow: '#d9a752',
+      lightBlue: '#5696b1',
+      lightMagenta: '#856389',
+      lightCyan: '#5f8f8b',
+      lightWhite: '#dfd8c9'
+    }
+  },
+
+  plugins: [],
+  localPlugins: []
+};
